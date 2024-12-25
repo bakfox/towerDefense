386e6c08f748111e132a244feafe845a9e33aa8d
yoon-H <ucando0320@gmail.com>
2024-12-25 21:08:16 +0900

    feat : add actionMappings

---
+++ public/src/handlers/actionMappings.js
@@ -0,0 +1,16 @@
+const actionMappings = {
+  4: 'changeStage',
+  5: 'changeHouseHp',
+  6: 'changeScore',
+  101: 'installTower',
+  102: 'moveTower',
+  103: 'sellTower',
+  104: 'moveTower',
+  105: 'towerAttack',
+  201: 'createMonster',
+  202: 'moveMonster',
+  203: 'monsterAttack',
+  204: 'killMonster'
+  };
+  
+  export default actionMappings;
