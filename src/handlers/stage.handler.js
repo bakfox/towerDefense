9954c16f5d8783e1567d61191e959b299783864d
bakfox <153157605+bakfox@users.noreply.github.com>
2024-12-26 19:01:56 +0900

    feat : stage.handler.js
    스테이지 관련 핸들러 들에게 클라이언트에 보낼 데이터 추가

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -14,9 +14,11 @@ export const gameStart = (payload) => {
   }
   console.log(nowMonsterData, nowStageData);
   return {
+    handlerId: 1,
     status: "succes",
     message: "게임을 시작합니다!",
     data: {
+      inGameData: inGame,
       nowStageData,
       nowMonsterData,
       towerData,
@@ -30,23 +32,66 @@ export const gameEnd = (payload) => {
 
 // 여기 아래는 서버에서 핸들러가 따로 없음 객체 형태로 보내기
 
-export const gameStageChange = (payload) => {
+export const gameStageChange = (socket) => {
   const inGame = getInGame(uuid);
   inGame.stage++;
+  const nextStageData = stageData.data[inGame.stage];
+  const nextMonsterData = [];
+  for (let index = 0; index < nowStageData.length; index++) {
+    nextMonsterData.push(monsterData.data[nowStageData[index].id]);
+  }
   console.log(inGame);
+  socket.emit("event", {
+    handlerId: 3,
+    status: "succes",
+    message: "스테이지 변경에 성공했습니다",
+    data: {
+      stage: inGame.stage,
+      nextStageData,
+      nextMonsterData,
+    },
+  });
 };
-export const gameHouseChange = (payload) => {
+export const gameHouseChange = (socket, damage) => {
   const inGame = getInGame(uuid);
-  inGame.hp -= payload.parsedData.damage;
+  inGame.hp -= damage;
   console.log(inGame);
+  socket.emit("event", {
+    handlerId: 4,
+    status: "succes",
+    message: "스테이지 변경에 성공했습니다",
+    data: {
+      stage: inGame.stage,
+    },
+  });
 };
-export const gameGoldChange = (payload) => {
+export const gameGoldChange = (socket, gold) => {
   const inGame = getInGame(payload.uuid);
-  inGame.gold += payload.parsedData.gold;
+  inGame.gold += gold;
   console.log(inGame);
+  socket.emit("event", {
+    handlerId: 5,
+    status: "succes",
+    message: "스테이지 변경에 성공했습니다",
+    data: {
+      stage: inGame.stage,
+      nextStageData,
+      nextMonsterData,
+    },
+  });
 };
-export const gameScoreChange = (payload) => {
+export const gameScoreChange = (socket, score) => {
   const inGame = getInGame(payload.uuid);
-  inGame.score += payload.parsedData.score;
+  inGame.score += score;
   console.log(inGame);
+  socket.emit("event", {
+    handlerId: 6,
+    status: "succes",
+    message: "스테이지 변경에 성공했습니다",
+    data: {
+      stage: inGame.stage,
+      nextStageData,
+      nextMonsterData,
+    },
+  });
 };
