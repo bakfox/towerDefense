5c684b479a9f53386ce98e761d3d1a4b4530bafa
Youmin Lee <youmin0107@naver.com>
2024-12-26 19:23:22 +0900

    fix: monsterData 받아오기

--- src/handlers/monsterHandler.js
+++ src/handlers/monsterHandler.js
@@ -1,8 +1,8 @@
 import { Monster } from "../src/monster.js";
-import {monsterData} from "../../gameDefaultData/monster.js";
+import monsterData from "../../gameDefaultData/monster.js";
 
 const monsters = []; // 몬스터를 저장할 배열
-
+let monsterData = monsterData;
 export const spawnMonster = (path, monsterImages, id,monsterData) => {
   const monster = new Monster(path, monsterImages, id,monsterData);
   monsters.push(monster);
