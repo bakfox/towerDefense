649d8b09a0f417caa020bfcc87d12ecc61ba9541
yoon-H <ucando0320@gmail.com>
2024-12-26 17:29:44 +0900

    feat : add gameManager

---
+++ public/src/gameManager.js
@@ -0,0 +1,19 @@
+export class GameManager {
+  static isGameOver = false;
+  static isInitGame = false;
+  static stage = 0;
+
+  static userGold = 0; // 유저 골드
+  static score = 0; // 게임 점수
+  static highScore = 0; // 기존 최고 점수수
+
+  static reset() {
+    this.isGameOver = false;
+    this.isInitGame = false;
+    this.stage = 0;
+
+    this.userGold = 0;
+    this.score = 0;
+    this.highScore = 0;
+  }
+}
