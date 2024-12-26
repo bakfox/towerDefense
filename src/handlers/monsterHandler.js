5b959c8cbdd3df9825a743b001cdecea303daa86
bakfox <153157605+bakfox@users.noreply.github.com>
2024-12-26 19:46:38 +0900

    feat : monsterHandler
    몬스터 앞으로 제작해야할 틀 느낌입니다!

--- src/handlers/monsterHandler.js
+++ src/handlers/monsterHandler.js
@@ -1,10 +1,21 @@
 import { Monster } from "../src/monster.js";
 import monsterData from "../../gameDefaultData/monster.js";
+import { getInGame } from "../models/inGame.js";
 
-const monsters = []; // 몬스터를 저장할 배열
+let nowMonsterData = []; // 스폰할 몬스터를 데이터 저장할 배열
+let nowStageData = []; // 스폰할 스테이지 데이터
+
+const stageChange = (inGame) => {
+  nowMonsterData = [];
+  nowStageData = stageData.data[inGame.stage];
+  for (let index = 0; index < nowStageData.length; index++) {
+    nowMonsterData.push(monsterData.data[nowStageData[index].id]);
+  }
+};
+
 let monsterData = monsterData;
-export const spawnMonster = (path, monsterImages, id,monsterData) => {
-  const monster = new Monster(path, monsterImages, id,monsterData);
+export const spawnMonster = (path, monsterImages, id, monsterData) => {
+  const monster = new Monster(path, monsterImages, id, monsterData);
   monsters.push(monster);
   return monster;
 };
@@ -23,6 +34,14 @@ export const moveMonsters = (base) => {
     }
   }
 };
+
+//몬스터 생성
+
+//몬스터 이동
+
+//몬스터 공격
+
+//몬스터 사망
 
 export const drawMonsters = (ctx) => {
   for (const monster of monsters) {
