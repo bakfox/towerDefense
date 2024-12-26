61734876d7b5920a518409064076b73661e7bc03
bakfox <153157605+bakfox@users.noreply.github.com>
2024-12-26 14:44:25 +0900

    fix : stageHandler
    핸들러 받는 데이터 수정

--- src/handlers/stageHandler.js
+++ src/handlers/stageHandler.js
@@ -1,35 +1,35 @@
 import { endLoop, startLoop } from "../gameLogic/serverGame.js";
 import { createInGame, getInGame } from "../models/inGame.js";
 
-export const gameStart = (uuid, payload) => {
-  const inGame = createInGame(uuid);
-  startLoop(inGame, uuid);
+export const gameStart = (payload) => {
+  const inGame = createInGame(payload.uuid);
+  startLoop(inGame, payload.uuid, payload.socket);
 };
 
-export const gameEnd = (uuid, payload) => {
-  const inGame = createInGame(uuid);
-  endLoop(inGame, uuid);
+export const gameEnd = (payload) => {
+  const inGame = createInGame(payload.uuid);
+  endLoop(inGame, payload.uuid);
 };
 
-// 여기 아래는 서버에서 핸들러가 따로 없음
+// 여기 아래는 서버에서 핸들러가 따로 없음 객체 형태로 보내기
 
-export const gameStageChange = (uuid, payload) => {
+export const gameStageChange = (payload) => {
   const inGame = getInGame(uuid);
   inGame.stage++;
   console.log(inGame);
 };
-export const gameHouseChange = (uuid, payload) => {
+export const gameHouseChange = (payload) => {
   const inGame = getInGame(uuid);
-  inGame.hp -= payload.damage;
+  inGame.hp -= payload.parsedData.damage;
   console.log(inGame);
 };
-export const gameGoldChange = (uuid, payload) => {
-  const inGame = getInGame(uuid);
-  inGame.gold += payload.gold;
+export const gameGoldChange = (payload) => {
+  const inGame = getInGame(payload.uuid);
+  inGame.gold += payload.parsedData.gold;
   console.log(inGame);
 };
-export const gameScoreChange = (uuid, payload) => {
-  const inGame = getInGame(uuid);
-  inGame.score += payload.score;
+export const gameScoreChange = (payload) => {
+  const inGame = getInGame(payload.uuid);
+  inGame.score += payload.parsedData.score;
   console.log(inGame);
 };
