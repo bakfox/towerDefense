2be7fc6dad6aae78060fe9c3cfd7657da99ecacd
yoon-H <ucando0320@gmail.com>
2024-12-31 00:58:20 +0900

    feat : add button class

---
+++ public/src/button.js
@@ -0,0 +1,27 @@
+class Button {
+  constructor(label, x, y, width, height, image = null, onClick) {
+    this.label = label;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    this.onClick = onClick;
+  }
+
+  // 마우스 클릭이 버튼 안에 있는지 확인
+  isClicked(mouseX, mouseY) {
+    return (
+      mouseX >= this.x &&
+      mouseX <= this.x + this.width &&
+      mouseY >= this.y &&
+      mouseY <= this.y + this.height
+    );
+  }
+
+  draw(ctx) {
+    if (image)
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    else ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
